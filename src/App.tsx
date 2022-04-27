686f9b9f8ab74a92e12497dabb9ba384286a9692
Bartosz Piech <piech.bartosz696@gmail.com>
2022-04-27 02:42:18 +0200

    added searchbar component and divs in the mainpage (TODO)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
+import SearchBox from './components/SearchBox/SearchBox';
 
 function App() {
   return (
     <div className="App">
         <Navbar title="PLAY2GETHER">
         </Navbar>
+        <SearchBox>
+        </SearchBox>
+        <div className="map" style={{float: "left", width: "50%"}}>
+            map
+        </div>
+        <div className="ad" style={{float: "left", width: "20%"}}>
+            ad
+        </div>
+        <div className="footer" style={{clear: "both"}}>
+            footer
+        </div>
     </div>
   );
 }

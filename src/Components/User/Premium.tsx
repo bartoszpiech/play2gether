a3ade920027818c2194833c928e78c38f4ce4dff
Bartosz Piech <piech.bartosz696@gmail.com>
2022-05-29 17:45:51 +0200

    repaired padding on user/premium website

--- src/Components/User/Premium.tsx
+++ src/Components/User/Premium.tsx
@@ -16,9 +16,9 @@ function Premium() {
         <div className="container-fluid d-flex flex-column rounded-3 shadow my-xl-5 my-0 p-5">
             <h1 className="display-1 text-center">Premium</h1>
 
-            <h4 className="my-5 border-bottom text-center">Brak reklam</h4>
-            <h4 className="my-5 border-bottom text-center">Dostęp do limitowanych miejsc</h4>
-            <h4 className="my-5 rainbow-text border-bottom text-center">Kolorowy nickname</h4>
+            <h4 className="my-2 border-bottom text-center">Brak reklam</h4>
+            <h4 className="my-2 border-bottom text-center">Dostęp do limitowanych miejsc</h4>
+            <h4 className="my-2 rainbow-text border-bottom text-center">Kolorowy nickname</h4>
 
             <div className="d-grid gap-2 mt-auto">
                 {account?.type === "premium" ? (
